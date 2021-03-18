a2ac4871a0ac2b7e14ba7c28a93ef8117759b2de
Andrew McAdams <andmcadams@gmail.com>
2021-03-18 17:54:07 -0500

    Cleanup, rate limiting, and first password check

--- index.js
+++ index.js
@@ -1,63 +1,72 @@
-const express = require('express')
-const sqlite3 = require('sqlite3')
-const app = express()
-const port = 3000
+const express = require("express");
+const sqlite3 = require("sqlite3");
+const rateLimit = require("express-rate-limit");
+const app = express();
+const port = 3000;
 
-let db = new sqlite3.Database('./stardb.db', sqlite3.OPEN_READWRITE)
+const db = new sqlite3.Database("./stardb.db", sqlite3.OPEN_READWRITE);
 
-function updateDb(loc, world, minTime, maxTime) {
-	// Check for worlds where world = this and maxTime is >
-	// this means there can't be another star already
-	let sql = `SELECT COUNT(*) FROM data WHERE world = ? AND maxTime > ?`
-	db.get(sql, [world, minTime], function(err, row) {
-		if (err) {
-			console.error(err)
-		}
-		if (row['COUNT(*)'] > 0)
-			console.log(`Already have this world: ${this.world}.`)
-		else {
-			console.log(`Adding row: ${this.loc}, ${this.world}, ${this.minTime}, ${this.maxTime}`)
-			db.run(`INSERT INTO data(location, world, minTime, maxTime) VALUES(?, ?, ?, ?)`, [this.loc, this.world, this.minTime, this.maxTime], function(err) {
-				if (err)
-					console.log('Error')
-			});
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 30,
+});
 
-		}
-	}.bind({loc: loc, world: world, minTime: minTime, maxTime: maxTime}))
+function updateDb(key, loc, world, minTime, maxTime) {
+  // Check for worlds where world = this and maxTime is >
+  // this means there can't be another star already
+  let sql = `SELECT COUNT(*) FROM data WHERE world = ? AND maxTime > ? and key = ?`;
+  db.get(
+    sql,
+    [world, minTime],
+    function (err, row) {
+      if (err) {
+        console.error(err);
+      }
+      if (row["COUNT(*)"] > 0)
+        console.log(`Already have this world: ${this.world}.`);
+      else {
+        console.log(
+          `Adding row: ${this.loc}, ${this.world}, ${this.minTime}, ${this.maxTime}`
+        );
+        db.run(
+          `INSERT INTO data(location, world, minTime, maxTime) VALUES(?, ?, ?, ?)`,
+          [this.loc, this.world, this.minTime, this.maxTime],
+          function (err) {
+            if (err) console.log("Error");
+          }
+        );
+      }
+    }.bind({ loc, world, minTime, maxTime })
+  );
 }
 
 app.use(express.json());
-app.post('/post', (req, res) => {
-	console.log(req.body)
-	for (let i in req.body)
-	{
-		let datapoint = req.body[i]
-		var loc = datapoint.loc
-		var world = datapoint.world
-		var minTime = datapoint.minTime
-		var maxTime = datapoint.maxTime
-		updateDb(loc, world, minTime, maxTime)
+app.use(limiter);
+app.post("/stars", (req, res) => {
+  console.log(req.body);
+  console.log(req.headers.authorization);
+  const key = req.headers.authorization;
+  for (let i in req.body) {
+    let datapoint = req.body[i];
+    const { loc, world, minTime, maxTime } = req.body[i];
+    updateDb(loc, world, minTime, maxTime);
+  }
+  return res.send("Shooting star data received");
+});
 
-	}
-	return res.send('Message received, thanks!')
-})
+app.get("/stars", (req, res) => {
+  let sql = `SELECT * FROM data where maxTime > ? ORDER BY minTime`;
+  db.all(sql, [Math.floor(Date.now() / 1000)], (err, row) => {
+    if (err) {
+      console.log("get error");
+      console.log(err);
+      return res.status(500);
+    }
 
-app.get('/getStars', (req, res) => {
-	let sql = `SELECT * FROM data where maxTime > ? ORDER BY minTime`
-	db.all(sql, [Math.floor(Date.now()/1000)], (err, row) => {
-		if (err) {
-			console.log('get error')
-			console.log(err)
-			return res.status(500)
-		}
-
-		return res.send(row)
-
-	});
-
+    return res.send(row);
+  });
 });
 
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
-})
-
+  console.log(`Example app listening at http://localhost:${port}`);
+});
